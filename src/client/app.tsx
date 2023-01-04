98ad38ee5b42c57bcffce9e953355b2c8f55f3ad
PlaguedLime <35981913+PlaguedLime@users.noreply.github.com>
2023-01-03 20:48:49 -0600

    Initial commit

---
+++ src/client/app.tsx
@@ -0,0 +1,15 @@
+import tw from "twin.macro"
+
+export default function App() {
+  return (
+    <AppWrapper data-testid="app-wrapper">
+      <AppContainer>
+        <h1>Your new app!</h1>
+        <p>Built with NodeJS, ExpressJS, React, MongoDB and Typescript</p>
+      </AppContainer>
+    </AppWrapper>
+  )
+}
+
+const AppWrapper = tw.div`w-screen min-h-screen flex flex-col items-center justify-center text-center`
+const AppContainer = tw.div`w-fit h-fit flex flex-col items-center justify-center gap-1 bg-orange-300 rounded-xl px-10 py-7 text-gray-900 shadow-lg border border-gray-900`
